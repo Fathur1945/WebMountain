4eb526c7b68c99ca45d56ea9b739bebf11b01257
Fathur1945 <142276371+Fathur1945@users.noreply.github.com>
2024-05-19 20:45:44 +0700

    Files upload

---
+++ index.php
@@ -0,0 +1,380 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./dist/css/style.css?v=<?php echo time(); ?>">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap" rel="stylesheet" />
+    <link href="image/Vektor_Desain_Logo_Gunung__Vektor__Desain__Simbol_PNG_dan_Vektor_dengan_Background_Transparan_untuk_Unduh_Gratis-removebg-preview.png" rel="icon"/>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+    <title>Mountain</title>
+  </head>
+  <body id="home">
+    <header>
+      <!-- navigation -->
+      <section class="navigation">
+        <div class="container">
+          <div class="box-navigation animate__animated animate__fadeInDown">
+            <div class="box">
+              <h1>Mountain</h1>
+            </div>
+            <div class="box menu-navigation" style="margin-right: 65px;">
+              <ul>
+                <li>
+                  <i class="ri-home-3-line"></i>
+                  <a href="#home">Beranda</a> 
+                </li>
+                <li>
+                  <i class="ri-information-line"></i>
+                  <a href="#about">About</a>
+                </li>
+                <li>
+                  <i class="ri-dashboard-line"></i>
+                  <a href="#famous">Famous</a>
+                </li>
+                <li>
+                  <i class="ri-image-line"></i>
+                  <a href="#gallery">Gallery</a>
+                </li>
+                <li>
+                  <i class="ri-phone-line"></i>
+                  <a href="#contact">Contact</a>
+                </li>
+              </ul>
+            </div>
+            <div class="box menu-bar">
+              <i class="ri-menu-3-fill" style="color: white;"></i>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- navigation -->
+
+      <!-- hero -->
+      <section class="hero">
+        <h1 class="animate__animated animate__pulse">Keindahan Gunung yang ada di seluruh Dunia</h1>
+      </section>
+      <!-- hero -->
+    </header>
+
+      <!-- about -->
+    <section class="about" id="about">
+      <div class="container">
+        <div class="box-about">
+          <div class="box" data-aos="fade-right" data-aos-duration="1000">
+            <h1>Mountain</h1>
+            <p>
+              website ini adalah platform online yang menyediakan informasi seputar gunung di seluruh dunia disertai dengan galeri yang menampilkan para pendaki yang sedang mendaki puncak gunung
+            </p>
+          </div>
+          <div class="box" data-aos="zoom-in" data-aos-duration="1000">
+            <img src="./image/1.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- about -->
+
+    <!-- famous -->
+<div class="container-famous" id="famous">
+<h1 class="heading"></h1>
+<div class="box-container">
+   <div class="box">
+      <div class="image">
+         <img src="./dist/css/images/everest-famous.jpg" alt="">
+      </div>
+      <div class="content">
+         <h3>Everest Mountain</h3>
+         <p>Gunung Everest ini menjulang setinggi 29.035 kaki yaitu sekitar lebih dari 8.800 MDPL yang diselimuti salju</p>
+         <a href="image-everest/Everest.html" class="btn">Read more</a>
+      </div>
+   </div>
+   <div class="box">
+      <div class="image">
+         <img src="./dist/css/images/fuji-famous.jpg" alt="">
+      </div>
+      <div class="content">
+         <h3>Fuji Mountain</h3>
+         <p>Gunung Fuji, terletak di pulau Honshu, adalah gunung tertinggi di Jepang, dengan ketinggian 3.776,24 m</p>
+         <a href="image-fuji/fuji.html" class="btn">Read more</a>
+      </div>
+   </div>
+   <div class="box">
+      <div class="image">
+         <img src="./dist/css/images/k2-famous.jpg" alt="">
+      </div>
+      <div class="content">
+         <h3>K2 Mountain</h3>
+         <p>K2 adalah gunung tertinggi ke-2 di dunia. Terletak di perbatasan antara Pakistan dan Republik Rakyat Tiongkok </p>
+         <a href="image-k2/k2.html" class="btn">Read more</a>
+      </div>
+   </div>
+   <div class="box">
+      <div class="image">
+         <img src="./dist/css/images/aconcagua.jpg" alt="">
+      </div>
+      <div class="content">
+         <h3>Aconcagua  Mountain</h3>
+         <p>Gunung Aconcagua berada di Argentina yang terkenal dengan cuaca dingin ekstrem lengkap dengan badain anginnya. </p>
+         <a href="image-aconcagua/Aconcagua.html" class="btn">Read more</a>
+      </div>
+   </div>
+   <div class="box">
+      <div class="image">
+         <img src="./dist/css/images/denali.jpg" alt="">
+      </div>
+      <div class="content">
+         <h3>Denali Mountain</h3>
+         <p>Gunung Denali berada di ketinggian 6.194 mdpl dan berada di kawasan Amerika Utara, tepatnya di Pegunungan Alaska. </p>
+         <a href="image-denali/denali.html" class="btn">Read more</a>
+      </div>
+   </div>
+   <div class="box">
+      <div class="image">
+         <img src="./dist/css/images/kilimajaro.jpg" alt="">
+      </div>
+      <div class="content">
+         <h3>Kilimanjaro Mountain</h3>
+         <p>Gunung Kilimanjaro adalah gunung tertinggi di dunia yang juga terkenal dengan misteri dan keindahannya. </p>
+         <a href="image-kilimanjiro/kilimanjaro.html" class="btn">Read more</a>
+      </div>
+   </div>
+   <div class="box">
+      <div class="image">
+         <img src="./dist/css/images/Elbrus.jpg" alt="">
+      </div>
+      <div class="content">
+         <h3>Elbrus Mountain</h3>
+         <p>Gunung Elbrus  adalah gunung tertinggi di Rusia dan Eropa . Terletak di bagian barat Kaukasus Utara dan merupakan puncak tertinggi Pegunungan Kaukasus . </p>
+         <a href="image-elbrus/elbrus.html" class="btn">Read more</a>
+      </div>
+   </div>
+   <div class="box">
+      <div class="image">
+         <img src="./dist/css/images/vinson.jpg" alt="">
+      </div>
+      <div class="content">
+         <h3>Vinson Mountain</h3>
+         <p>Vinson Massif adalah gunung terbesar di Antartika, dan memiliki ukuran 21 km (13 mi) panjang dan lebar 13 km (8 mi) dan terletak di dalam Sentinel Range dari Pegunungan Ellsworth.</p>
+         <a href="image-vinson/vinson.html" class="btn">Read more</a>
+      </div>
+   </div>
+   <div class="box">
+      <div class="image">
+         <img src="./dist/css/images/jaya.jpg" alt="">
+      </div>
+      <div class="content">
+         <h3>Jaya Mountain</h3>
+         <p>Puncak Jaya Wijaya atau Nemangkawi Ninggok ialah sebuah puncak tertinggi yang menjadi bagian dari Pegunungan Barisan Sudirman yang terdapat di Kabupaten Mimika, Provinsi Papua Tengah, Indonesia.</p>
+         <a href="image-jaya/jaya.html" class="btn">Read more</a>
+      </div>
+   </div>
+   <div class="box">
+      <div class="image">
+         <img src="./dist/css/images/helens.jpg" alt="">
+      </div>
+      <div class="content">
+         <h3>St. Helens Mountain</h3>
+         <p>Gunung St. Helens adalah sebuah gunung berapi aktif yang terletak di Skamania County, Washington, Amerika Serikat.</p>
+         <a href="image-helens/helens.html" class="btn">Read more</a>
+      </div>
+   </div>
+   <div class="box">
+      <div class="image">
+         <img src="./dist/css/images/rinjani.jpg" alt="">
+      </div>
+      <div class="content">
+         <h3>Rinjani Mountain</h3>
+         <p>Gunung Rinjani adalah gunung yang berlokasi di Pulau Lombok, Nusa Tenggara Barat.  gunung berapi kedua tertinggi di Indonesia dengan ketinggian 3.726 mdpl </p>
+         <a href="image-rinjani/rinjani.html" class="btn">Read more</a>
+      </div>
+   </div>
+   <div class="box">
+      <div class="image">
+         <img src="./dist/css/images/blanc.jpg" alt="">
+      </div>
+      <div class="content">
+         <h3>Blanc Mountain</h3>
+         <p>Gunung Blanc adalah gunung tertinggi di Eropa barat dengan ketinggian sekitar 4.810 meter. Gunung ini bagian dari Pegunungan Alpen.</p>
+         <a href="image-blanc/blanc.html" class="btn">Read more</a>
+      </div>
+   </div>
+   <div class="box">
+      <div class="image">
+         <img src="./dist/css/images/Vesuvius.jpg" alt="">
+      </div>
+      <div class="content">
+         <h3>Vesuvius Mountain</h3>
+         <p>Gunung Vesuvius adalah satu-satunya gunung berapi aktif di Eropa Daratan yang terletak di sebelah timur Napoli, Italia.</p>
+         <a href="image-vesuvius/Vesuvius.html" class="btn">Read more</a>
+      </div>
+   </div>
+   <div class="box">
+      <div class="image">
+         <img src="./dist/css/images/matterhorn.jpg" alt="">
+      </div>
+      <div class="content">
+         <h3>Matterhorn Mountain</h3>
+         <p>Matterhorn adalah sebuah gunung terpencil. Karena posisinya di daerah aliran sungai Alpine utama dan tinggi yang hebat, Matterhorn menghadapi perubahan cuaca yang cepat </p>
+         <a href="image-matterhorn/Matterhorn.html" class="btn">Read more</a>
+      </div>
+   </div>
+   <div class="box">
+      <div class="image">
+         <img src="./dist/css/images/annapurna.jpg" alt="">
+      </div>
+      <div class="content">
+         <h3>Annapurna Mountain</h3>
+         <p>Annapurna adalah salah satu gunung tertinggi didunia yang berada di Pegunungan Himalaya. Gunung ini memiliki delapan buah puncak</p>
+         <a href="image-annapurna/Annapurna.html" class="btn">Read more</a>
+      </div>
+   </div>
+   <div class="box">
+      <div class="image">
+         <img src="./dist/css/images/mauna.jpg" alt="">
+      </div>
+      <div class="content">
+         <h3>Mauna Kea Mountain</h3>
+         <p>Mauna Kea  adalah Gunung berapi tidur di pulau Hawaii . Puncaknya berada pada ketinggian 4.207,3 m (13.803 ft) di atas permukaan laut,</p>
+         <a href="image-mauna/Mauna.html" class="btn">Read more</a>
+      </div>
+   </div>
+   <div class="box">
+      <div class="image">
+         <img src="./dist/css/images/raniner.jpg" alt="">
+      </div>
+      <div class="content">
+         <h3>Rainier Mountain</h3>
+         <p>Gunung Rainier adalah sebuah gunung berapi kerucut aktif di Cascadia yang terletak 95 mil (95 km) selatan-tenggara Seattle,</p>
+         <a href="image-rainer/rainer.html" class="btn">Read more</a>
+      </div>
+   </div>
+   <div class="box">
+      <div class="image">
+         <img src="./dist/css/images/lhotse.jpg" alt="">
+      </div>
+      <div class="content">
+         <h3>Lhotse Mountain</h3>
+         <p>Lhotse  merupakan sebuah gunung di Nepal dan RRC. Gunung ini terletak di bagian utara di negara itu </p>
+         <a href="image-lhotse/lhotse.html" class="btn">Read more</a>
+      </div>
+   </div>
+</div>
+<div id="load-more"> Load more </div>
+</div>
+    <!-- famous -->
+
+    <!-- gallery -->
+    <section class="gallery" id="gallery">
+      <div class="wrapper">
+        <div class="containergaler">
+            <input type="radio" name="slide" id="c1" checked>
+            <label for="c1" class="card">
+                <div class="row">
+                    <div class="icon">1</div>
+                    <div class="description">
+                        <h4></h4>
+                    </div>
+                </div>
+            </label>
+            <input type="radio" name="slide" id="c2" >
+            <label for="c2" class="card">
+                <div class="row">
+                    <div class="icon">2</div>
+                    <div class="description">
+                        <h4></h4>
+                    </div>
+                </div>
+            </label>
+            <input type="radio" name="slide" id="c3" >
+            <label for="c3" class="card">
+                <div class="row">
+                    <div class="icon">3</div>
+                    <div class="description">
+                        <h4></h4>
+                    </div>
+                </div>
+            </label>
+            <input type="radio" name="slide" id="c4" >
+            <label for="c4" class="card">
+                <div class="row">
+                    <div class="icon">4</div>
+                    <div class="description">
+                        <h4></h4>
+                    </div>
+                </div>
+            </label>
+        </div>
+    </div>
+  </section>
+    <!-- gallery -->
+
+    <!-- contact -->
+    <section class="contact" id="contact">
+      <div class="container contact ">
+        <div class="box-contact">
+        <form id="contact" action="./phpmailer/mail.php" method="post">
+          <h1 data-aos="fade-right" data-aos-duration="1000" data-aos-delay="100">Contact</h1>
+          <form action="" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="600">
+            <table>
+              <tr>
+                <td><label for="name">Nama</label></td>
+                <td><input type="text" id="nama" name="name" placeholder="Masukkan Nama" autocomplete="off" /></td>
+              </tr>
+              <tr>
+                <td><label for="email">Email</label></td>
+                <td><input type="email" id="email" name="email" placeholder="Masukkan Email" autocomplete="off" /></td>
+              </tr>
+              <td><label for="subject">subject</label></td>
+              <td><input type="subject" id="subject" name="subject" placeholder="Masukan subject" autocomplete="off" /></td>
+              </tr>
+              <tr>
+                <td><label for="message">pesan</label></td>
+                <td><textarea name="message" id="pesan" placeholder="Masukkan Pesan..." autocomplete="off"></textarea></td> 
+              </tr>
+            </table>
+            <button class="kontak"  type="submit" name="send" id="contact-submit">Submit Now</button>
+           </fieldset>
+          </form>
+          </form>
+        </div>
+      </div>
+    </section>
+    <!-- contact -->
+
+    <!-- footer -->
+    <footer>
+      <p>&copy; 2024 by Fathurrohman</p>
+    </footer>
+    <!-- footer -->
+
+    <script src="./dist/js/script.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+      AOS.init();
+    </script>
+    <script>
+
+let loadMoreBtn = document.querySelector('#load-more');
+let currentItem = 3;
+
+loadMoreBtn.onclick = () =>{
+   let boxes = [...document.querySelectorAll('.container-famous .box-container .box')];
+   for (var i = currentItem; i < currentItem + 3; i++){
+      boxes[i].style.display = 'inline-block';
+   }
+   currentItem += 3;
+
+   if(currentItem >= boxes.length){
+      loadMoreBtn.style.display = 'none';
+   }
+}
+
+</script>
+  </body>
+</html>
